4df68cdc0ef98488ba4710040496f840dc2d4619
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-21 23:00:03 +0700

    add image

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { supabase } from "$lib/supabaseClient";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-     const { data } = await supabase.from('contents').select().limit(20);
+     const { data } = await supabase.from('contents').select().order('id', { ascending: false }).limit(20);
      return {
           contents: data ?? []
      }
--- src/routes/h/[code]/+page.svelte
+++ src/routes/h/[code]/+page.svelte
@@ -4,4 +4,11 @@
      export let data: PageServerData
 </script>
 
+<img class="img" src="https://overdoujin.gumlet.io/covers/{data.content.code}" alt="{data.content.title}">
 <p>{ data.content.code }</p>
+
+<style>
+     .img {
+          max-width: 300px;
+     }
+</style>
