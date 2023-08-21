34f42a276c847a5036a82888a2193b8bec5eb000
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-21 23:22:13 +0700

    test optimizing image

--- src/routes/h/[code]/+page.svelte
+++ src/routes/h/[code]/+page.svelte
@@ -2,9 +2,12 @@
 	import type { PageServerData } from "./$types";
 
      export let data: PageServerData
+
+     let urlImage = `https://overdoujin.gumlet.io/covers/${data.content.code}`
+     let srcsetImage = `${urlImage}?w=250&format=webp&q=80&fit=fill 250w, ${urlImage}?w=300&format=webp&q=80&fit=fill 300w, ${urlImage}?w=600&format=webp&q=80&fit=fill 600w`
 </script>
 
-<img class="img" src="https://overdoujin.gumlet.io/covers/{data.content.code}" alt="{data.content.title}">
+<img class="img" srcset="{srcsetImage}" src="{urlImage}" alt="{data.content.title}">
 <p>{ data.content.code }</p>
 
 <style>
