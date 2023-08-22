68d16ce850f4e1846b73cc746801f30be0dbbe35
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-23 00:21:10 +0700

    debug r2

--- src/routes/h/[code]/+page.server.ts
+++ src/routes/h/[code]/+page.server.ts
@@ -11,13 +11,14 @@ export const load: PageServerLoad = async ({ params }) => {
 
      const listObjectsResponse = await s3.send(new ListObjectsV2Command({ Bucket: 'overdoujin', Prefix: `contents/${params.code}/` }))
     
-     const metadataPromises = listObjectsResponse?.Contents?.map(async (object) => {
-          const metadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: object.Key }))
-          return { key: object.Key, metadata: metadata.Metadata};
-     });
+     // const metadataPromises = listObjectsResponse?.Contents?.map(async (object) => {
+     //      const metadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: object.Key }))
+     //      return { key: object.Key, metadata: metadata.Metadata};
+     // });
 
-     const images = await Promise.all(metadataPromises);
+     // const images = await Promise.all(metadataPromises);
      return {
           content: data ?? [],
+          list: listObjectsResponse ?? [],
      }
 }
