fc9d5146e75cd5541d4eef55655b13134954da27
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-23 15:36:50 +0700

    debug pages 0.5

--- src/routes/h/[code]/+page.server.ts
+++ src/routes/h/[code]/+page.server.ts
@@ -16,6 +16,8 @@ export const load: PageServerLoad = async ({ params }) => {
      //      return { key: object.Key, metadata: metadata.Metadata};
      // });
 
+     const metadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: 'contents/unofg/01_unofg.png' }))
+
      // const images = await Promise.all(metadataPromises);     
 
      return {
