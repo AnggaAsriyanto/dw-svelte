d5cce6eee383d32a2b30240c7f1d2669f6747f93
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-23 14:50:04 +0700

    debug pages 0.1

--- src/routes/h/[code]/+page.server.ts
+++ src/routes/h/[code]/+page.server.ts
@@ -9,20 +9,17 @@ import {
 export const load: PageServerLoad = async ({ params }) => {
      const { data } = await supabase.from('contents').select('*').eq("code", params.code).single();
 
-     // const listObjectsResponse = await s3.send(new ListObjectsV2Command({ Bucket: 'overdoujin', Prefix: `contents/${params.code}/` }))
+     const listObjectsResponse = await s3.send(new ListObjectsV2Command({ Bucket: 'overdoujin', Prefix: `contents/${params.code}/` }))
     
-     // const metadataPromises = listObjectsResponse?.Contents?.map(async (object) => {
-     //      const metadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: object.Key }))
-     //      return { key: object.Key, metadata: metadata.Metadata};
-     // });
+     const metadataPromises = listObjectsResponse?.Contents?.map(async (object) => {
+          const metadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: object.Key }))
+          return { key: object.Key, metadata: metadata.Metadata};
+     });
 
-     // const images = await Promise.all(metadataPromises);
-
-     const testMetadata = await s3.send(new HeadObjectCommand({ Bucket: 'overdoujin', Key: 'contents/ncyxi/01_ncyxi.jpg' }))
-     
+     // const images = await Promise.all(metadataPromises);     
 
      return {
           content: data ?? [],
-          list: testMetadata ?? [],
+          list: listObjectsResponse ?? [],
      }
 }
