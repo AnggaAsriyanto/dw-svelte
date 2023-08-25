d8c379919383e1017fc466ceca31154c2ef520d0
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-25 15:20:43 +0700

    minimize output test

--- src/routes/h/[code]/+page.svelte
+++ src/routes/h/[code]/+page.svelte
@@ -11,7 +11,7 @@
 
 <!-- <img class="img" srcset="{srcsetImage}" src="{urlImage}" alt="{data.content.title}"> -->
 <p>{ data.content.code }</p>
-<p>{ data?.test }</p>
+<p>{ data?.test?.objects }</p>
 <!-- <ul>
      {#each data.images as image}
           <li>
