7449688901fe66569c444e2e4624aa549b8bd202
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-26 15:48:27 +0700

    back route

--- src/routes/hentai/[code]/[idx]/+page.svelte
+++ src/routes/hentai/[code]/[idx]/+page.svelte
@@ -4,20 +4,24 @@
      export let data;
      console.log(data)
 
-     // let nextLink = `/hentai/${data.code}/${parseInt(data.idx) + 1}`
-     // let prevLink = `/hentai/${data.code}/${parseInt(data.idx) - 1}`
-
      function nextIdx() {
+          if(data.idx === data.images.length) {
+               return goto(`/hentai/${data.code}`)
+          }
+
           goto(`/hentai/${data.code}/${data.idx + 1}`, { replaceState: true })
      }
 
      function prevIdx() {
+          if(data.idx === 1) {
+               return goto(`/hentai/${data.code}`)
+          }
           goto(`/hentai/${data.code}/${data.idx - 1}`, { replaceState: true })
      }
 </script>
 
 <p>Hello index { data.idx }</p>
-<button on:click={nextIdx}>next</button>
 <button on:click={prevIdx}>prev</button>
+<button on:click={nextIdx}>next</button>
 
 
