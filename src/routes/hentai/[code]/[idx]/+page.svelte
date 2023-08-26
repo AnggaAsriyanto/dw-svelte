3a349e515240564fb5ca23030fb991af9dcde3f8
AnggaNellow <anggazxcasd1@gmail.com>
2023-08-26 15:38:33 +0700

    try remove invalidate

--- src/routes/hentai/[code]/[idx]/+page.svelte
+++ src/routes/hentai/[code]/[idx]/+page.svelte
@@ -8,11 +8,11 @@
      // let prevLink = `/hentai/${data.code}/${parseInt(data.idx) - 1}`
 
      function nextIdx() {
-          goto(`/hentai/${data.code}/${data.idx + 1}`, { replaceState: true, invalidateAll: true })
+          goto(`/hentai/${data.code}/${data.idx + 1}`, { replaceState: true })
      }
 
      function prevIdx() {
-          goto(`/hentai/${data.code}/${data.idx - 1}`, { replaceState: true, invalidateAll: true })
+          goto(`/hentai/${data.code}/${data.idx - 1}`, { replaceState: true })
      }
 </script>
 
