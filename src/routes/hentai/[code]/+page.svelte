98114ab49f20759a9ca309e864015f7f81c96759
AnggaNellow <anggazxcasd1@gmail.com>
2023-09-21 22:51:24 +0700

    push force

--- src/routes/hentai/[code]/+page.svelte
+++ src/routes/hentai/[code]/+page.svelte
@@ -6,7 +6,6 @@
 
      console.log(data)
 
-     // let images = JSON.parse(data.test)
      let urlImage = `https://overdoujin.gumlet.io/covers/${data.content.code}`
      let srcsetImage = `${urlImage}?w=250&format=webp&q=80&fit=fill 250w, ${urlImage}?w=300&format=webp&q=80&fit=fill 300w, ${urlImage}?w=600&format=webp&q=80&fit=fill 600w`
 </script>
