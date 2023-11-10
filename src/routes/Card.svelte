b4fd1338bb77700a0f6949bb71c96fb5063efda9
AnggaNellow <anggazxcasd1@gmail.com>
2023-11-11 00:25:11 +0700

    add flag image

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -39,7 +39,7 @@
                </div>
                <div class="post-title">
                     <div class="title">
-                         <!-- <img src="/{post.language}.png" alt="{post.language}" width="18" height="18"> -->
+                         <img src="/{post.language}.png" alt="{post.language}" width="18" height="18">
                          <div>
                               <h3>{ post.title }</h3>
                          </div>
